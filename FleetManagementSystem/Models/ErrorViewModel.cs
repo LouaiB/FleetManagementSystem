0002f0a8f0365520bf24a09d3682618210a9d08d
LouaiB <jagaimosarad@gmail.com>
2019-04-07 18:33:12 +0300

    Add project files.

---
+++ FleetManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FleetManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
