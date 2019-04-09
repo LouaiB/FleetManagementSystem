99fb42a08d8fe580a9173243cb5b5779b3721ba6
AbdullahHaidar <theahmaster7@gmail.com>
2019-04-09 14:08:53 +0200

    Change Website

---
+++ FleetManagementWebApp/FleetManagementWebApp/Models/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FleetManagementWebApp.Models
+{
+    public class Company
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
